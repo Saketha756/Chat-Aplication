732afce7b4c7f53dc1a05460d88994a1f48c22a6
saketha pulasthi <saketha756@gmail.com>
2021-12-09 13:45:11 +0530

    reat app

---
+++ client/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {StreamChat} from 'stream-chat';
+import {Chat} from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+
+import {ChannelListContainer, ChannelContainer,Auth} from './components';
+
+import './App.css';
+
+const apikey = '4nvryratdmrv';
+
+const client = StreamChat.getInstance(apikey);
+
+const authToken  = false;
+
+const App = () => {
+    
+        if (!authToken) return <Auth />
+
+    return (
+        <div className="app__wrapper">
+            <Chat client={client} theme="team light">
+                <ChannelListContainer
+                />
+                <ChannelContainer
+                />
+            </Chat>
+        </div>
+    );
+    
+}
+export default App;
